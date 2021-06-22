f072b0140042dbce7942bc2a22bb32781ef3639d
manisha2307 <vallerimanisha2307@gmail.com>
2021-06-22 13:33:16 +0530

    first

--- arith.java
+++ arith.java
@@ -1,6 +1,16 @@
 /*basic arithmetic operations*/
-class arithmetic{
-    public Static void main(string []args)
-    
-
+import java.util.Scanner;
+public class addition {
+    public static void main(String[] args){
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number:");
+        num1= sc.nextInt();
+        System.out.println("enter second number:");
+        num2= sc.nextInt();
+        sc.close();
+        sum=num1+num2;
+        System.out.println("sum of the numbers:");
+    }
 }
+
